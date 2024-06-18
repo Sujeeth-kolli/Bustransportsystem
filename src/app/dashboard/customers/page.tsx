781ce388bbab1c3df027cf543ccac66fe27eee5f
Samarthanand Kamble <samarthanand.kamble@webilapps.com>
2024-06-18 11:10:01 +0530

    11.10 am

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -7,8 +7,8 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import { config } from '@/config';
-import { CustomersFilters } from '@/components/dashboard/customer/customers-filters';
-import { CustomersTable, type Customer } from '@/components/dashboard/customer/customers-table';
+import { CustomersFilters } from '@/components/dashboard/bus-pass-details/customers-filters';
+import { CustomersTable, type Customer } from '@/components/dashboard/bus-pass-details/customers-table';
 
 export const metadata = { title: `Customers | Dashboard | ${config.site.name}` };
 
--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -2,7 +2,7 @@ import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
 export const navItems = [
-  { key: 'overview', title: 'Dashboard', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'customers', title: 'Bus Pass Details', href: paths.dashboard.customers, icon: 'users' },
-  { key: 'integrations', title: 'Routes', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  { key: 'dashboard', title: 'Dashboard', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'buspass', title: 'Bus Pass Details', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'routes', title: 'Routes', href: paths.dashboard.integrations, icon: 'plugs-connected' },
 ] satisfies NavItemConfig[];
